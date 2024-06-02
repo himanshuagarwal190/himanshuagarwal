581d5fc930b63e34540c2e3b746784be74792634
Himanshu Agarwal <himanshuagarwal190@gmail.com>
2024-06-02 23:42:43 +0530

    intro component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Intro from "@/components/Intro";
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mx-16">
       <NavBar active={0} />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro.tsx
@@ -0,0 +1,13 @@
+export default function Intro(){
+    return <div className="flex mt-10 items-center justify-center">
+        <div className="w-1/2 flex justify-center items-center space-y-4 flex-col">
+            <p className="m-auto text-5xl">{"</Himanshu Agarwal >"}</p>
+            <p>Hey there! I'm a passionate software engineer dedicated to transforming innovative ideas into reality through code. With a blend of creativity and technical prowess, I specialize in crafting robust, user-centric applications that not only solve problems but also delight users. Dive into my world of software development, where every line of code is a step towards a smarter, more connected future. Let's build something amazing together!</p>
+        </div>
+        <div className="w-1/2">
+            <div className="m-auto me-image box-content rounded-full">
+                <img className="w-full h-full" src="./me.png" alt="" />
+            </div>
+        </div>
+    </div>
+}
